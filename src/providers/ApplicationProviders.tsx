b66b72813e35ae94647e7a317bf7a90ede6a7a87
Dobbymin <gmin990624@gmail.com>
2025-06-27 22:45:45 +0900

    feat: alias 적용

--- src/providers/ApplicationProviders.tsx
+++ src/providers/ApplicationProviders.tsx
@@ -1,6 +1,7 @@
-import { queryClient } from '@/libs';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { queryClient } from '@/libs';
 
 type Props = {
   children: React.ReactNode;
