60d77e773b41117a30a2f777f124385e5cba55ff
Dobbymin <gmin990624@gmail.com>
2025-06-28 03:27:41 +0900

    feat: @emotion/babel-plugin 추가 및 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
-
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      plugins: [['@emotion/babel-plugin', {}]],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
