6f84fa3c9f47261212408021913445adb77c3cee
Dobbymin <gmin990624@gmail.com>
2025-07-04 01:19:56 +0900

    feat(login): login dialog 구현

---
+++ src/features/main/ui/dialog/LoginDialog.tsx
@@ -0,0 +1,102 @@
+import { useForm } from 'react-hook-form';
+
+import styled from '@emotion/styled';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+
+import type { LoginSchemaType } from '@/entities';
+import { loginSchema } from '@/entities';
+import {
+  Button,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  Form,
+} from '@/shared';
+
+import { loginAPI } from '../../apis';
+import { LoginButton, PasswordField, UserIdField } from '../../components';
+
+export const LoginDialog = () => {
+  const { mutate: loginMutate } = useMutation({
+    mutationFn: async (data: LoginSchemaType) => {
+      return loginAPI({
+        userId: data.userId,
+        password: data.password,
+      });
+    },
+    onSuccess: () => {
+      console.log('login success');
+    },
+    onError: () => {
+      console.log('login error');
+    },
+  });
+
+  const form = useForm<LoginSchemaType>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      userId: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: LoginSchemaType) => {
+    loginMutate(data);
+
+    form.reset();
+  };
+
+  return (
+    <Dialog>
+      <Form {...form}>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <LoginButton />
+          <DialogContentContainer>
+            <DialogHeader>
+              <DialogTitle>로그인</DialogTitle>
+            </DialogHeader>
+            <FieldContainer>
+              <UserIdField />
+              <PasswordField />
+            </FieldContainer>
+            <DialogFooter>
+              <ButtonContainer>
+                <Button
+                  type='submit'
+                  disabled={!form.formState.isValid}
+                  onClick={form.handleSubmit(onSubmit)}
+                >
+                  로그인
+                </Button>
+                <DialogClose asChild>
+                  <Button variant='outline'>취소</Button>
+                </DialogClose>
+              </ButtonContainer>
+            </DialogFooter>
+          </DialogContentContainer>
+        </form>
+      </Form>
+    </Dialog>
+  );
+};
+const DialogContentContainer = styled(DialogContent)`
+  @media (min-width: 640px) {
+    max-width: 425px;
+  }
+`;
+
+const FieldContainer = styled.div`
+  display: grid;
+  gap: 1.5rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
+  width: 100%;
+`;
