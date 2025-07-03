d2822bdd9e58b1fc5ea95a8eca34c377c5b02a59
Dobbymin <gmin990624@gmail.com>
2025-07-04 01:21:04 +0900

    feat(main): login dialog 구현 및 적용

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,3 +1,20 @@
+import styled from '@emotion/styled';
+
+import { LoginDialog } from '@/features';
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 export default function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <Container>
+      <LoginDialog />
+    </Container>
+  );
 }
