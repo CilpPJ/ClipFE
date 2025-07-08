abe489573a362825cfa4941a6d40e3ea79270d93
Dobbymin <gmin990624@gmail.com>
2025-07-08 21:12:36 +0900

    refactor: layout 구현 및 코드 분리

--- src/shared/components/features/layouts/index.ts
+++ src/shared/components/features/layouts/index.ts
@@ -1,1 +1,2 @@
 export * from './NavigateBar';
+export * from './Header';
---
+++ src/widgets/layout/Layout.tsx
@@ -0,0 +1,35 @@
+import { Outlet } from 'react-router-dom';
+
+import styled from '@emotion/styled';
+
+import { HEADER_HEIGHT, Header, NavigateBar, ScrollToTop } from '@/shared';
+
+export const Layout = () => {
+  return (
+    <PageLayout>
+      <ScrollToTop />
+      <Header />
+      <PageContainer>
+        <Outlet />
+      </PageContainer>
+      <NavigateBar />
+    </PageLayout>
+  );
+};
+
+const PageLayout = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+const PageContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-top: ${HEADER_HEIGHT}px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
