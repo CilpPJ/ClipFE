d009f9cba7b6e62ff1978d52fd2f8ca1425a21a5
Dobbymin <gmin990624@gmail.com>
2025-07-23 20:24:18 +0900

    feat(header): 헤더 컴포넌트에 로고 및 제목 추가, 로그인/회원가입 버튼으로 수정

--- src/shared/components/features/layouts/Header.tsx
+++ src/shared/components/features/layouts/Header.tsx
@@ -1,16 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
 import styled from '@emotion/styled';
 
-import { LoginDialog, SignupDialog } from '@/features';
+import Clip_Logo from '../../../_assets/logo/clip_logo_2.webp';
+import { ROUTER_PATH } from '../../../constants';
+import { Button } from '../../common';
 
 export const HEADER_HEIGHT = 60;
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <HeaderLayout>
-      <h1>CLIP</h1>
+      <TitleWrapper>
+        <LogoImage src={Clip_Logo} alt='Clip Logo' />
+        <TitleText>Clip</TitleText>
+      </TitleWrapper>
+      {/* TODO: 임시로 추가한 부분이며 추후 제거해야함 */}
       <HeaderButtonWrapper>
-        <LoginDialog />
-        <SignupDialog />
+        <Button variant='outline' onClick={() => navigate(ROUTER_PATH.LOGIN)}>
+          로그인
+        </Button>
+        <Button variant='outline' onClick={() => navigate(ROUTER_PATH.SIGN_UP)}>
+          회원가입
+        </Button>
       </HeaderButtonWrapper>
     </HeaderLayout>
   );
@@ -23,10 +37,25 @@ const HeaderLayout = styled.header`
   padding: 1rem;
   align-items: center;
   justify-content: space-between;
-  gap: 1rem;
+  gap: 8px;
 `;
 
 const HeaderButtonWrapper = styled.div`
   display: flex;
   gap: 1rem;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoImage = styled.img`
+  width: 2rem;
+  height: 2rem;
+`;
+
+const TitleText = styled.h1`
+  font-size: 1.5rem;
+  font-family: 'bnviitlasik';
 `;
