cf68dc23cd769ca24c10f9c037b217ed991a70bc
Dobbymin <gmin990624@gmail.com>
2025-07-23 21:51:55 +0900

    feat(LoginTitleSection): 로고 이미지 추가 및 텍스트 형식 수정

--- src/entities/auth/ui/section/LoginTitleSection.tsx
+++ src/entities/auth/ui/section/LoginTitleSection.tsx
@@ -1,10 +1,15 @@
 import styled from '@emotion/styled';
 
+import Logo from '@/shared/_assets/logo/clip_3d.webp';
+
 export const LoginTitleSection = () => {
   return (
     <LoginTitleSectionBox>
+      <LogoImage src={Logo} alt='Clip Logo' />
       <TitleText>우리만의 지식 네트워크</TitleText>
-      <TitleText>Clip에서 함께 시작하세요!</TitleText>
+      <TitleText>
+        <LogoText>Clip</LogoText>에서 함께 시작하세요!
+      </TitleText>
     </LoginTitleSectionBox>
   );
 };
@@ -21,5 +26,17 @@ const TitleText = styled.p`
   font-size: 18px;
   font-weight: bold;
   color: #333;
-  margin-bottom: 16px;
+  margin-bottom: 12px;
+`;
+const LogoText = styled.span`
+  font-size: 20px;
+  font-family: 'BnviitLasik';
+  color: var(--secondary);
+  padding: 0 4px;
+`;
+
+const LogoImage = styled.img`
+  width: 100px;
+  height: 100px;
+  margin: 2rem 0;
 `;
