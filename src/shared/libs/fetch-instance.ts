60a51ca146e595d0c75b18f071da291dfd98bf05
Dobbymin <gmin990624@gmail.com>
2025-08-10 21:34:38 +0900

    feat: 요청 인터셉터 추가 및 세션 만료 시 처리 로직 구현

--- src/shared/libs/fetch-instance.ts
+++ src/shared/libs/fetch-instance.ts
@@ -1,6 +1,11 @@
+import { toast } from 'sonner';
+
+import { ROUTER_PATH } from '../constants';
+import { useAuthStore } from '../store';
+
 import { initInstance } from './axios-instance';
 
-export const BASE_URL = 'https://www.clipin.store';
+export const BASE_URL = 'http://localhost:8080';
 
 export const fetchInstance = initInstance({
   baseURL: BASE_URL,
@@ -11,3 +16,24 @@ export const fetchInstance = initInstance({
     'Content-Type': 'application/json',
   },
 });
+
+fetchInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    if (error.response?.status !== 500) {
+      return Promise.reject(error);
+    }
+
+    const { clearNickname } = useAuthStore.getState();
+
+    clearNickname();
+
+    toast.error('세션이 만료되었습니다. 다시 로그인해주세요.');
+
+    window.location.href = ROUTER_PATH.LOGIN;
+
+    return Promise.reject(error);
+  },
+);
