cae8d20dac13b93eaaa2ce5b070a11848495ac6e
Dobbymin <gmin990624@gmail.com>
2025-08-11 22:29:37 +0900

    refactor: auth store를 accessToken 관리로 변경 및 관련 타입 정의 수정

--- src/shared/store/useAuth.ts
+++ src/shared/store/useAuth.ts
@@ -1,26 +1,20 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
-import type { User } from '../types';
-
-// User type에서 nickname만 가져와서 사용
-type NicknameState = Partial<Pick<User, 'nickname'>>;
-
-type NicknameActions = {
-  setNickname: (nickname: string) => void;
-  clearNickname: () => void;
+// accessToken을 메모리에서만 관리하는 store
+type AccessTokenState = {
+  accessToken: string | null;
 };
 
-export const useAuthStore = create(
-  persist<NicknameState & NicknameActions>(
-    (set) => ({
-      nickname: undefined,
+type AccessTokenActions = {
+  setAccessToken: (token: string) => void;
+  clearAccessToken: () => void;
+};
 
-      setNickname: (nickname) => set({ nickname }),
-      clearNickname: () => set({ nickname: undefined }),
-    }),
-    {
-      name: 'nickname',
-    },
-  ),
+export const useAuthStore = create<AccessTokenState & AccessTokenActions>(
+  (set) => ({
+    accessToken: null,
+
+    setAccessToken: (accessToken) => set({ accessToken }),
+    clearAccessToken: () => set({ accessToken: null }),
+  }),
 );
