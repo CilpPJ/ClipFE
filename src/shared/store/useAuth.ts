3fe557ae54d02d1853a045548d609afdf529be27
Dobbymin <gmin990624@gmail.com>
2025-08-14 12:22:02 +0900

    refactor: zustand devtools 적용

--- src/shared/store/useAuth.ts
+++ src/shared/store/useAuth.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 // accessToken을 메모리에서만 관리하는 store
 type AccessTokenState = {
@@ -10,11 +11,18 @@ type AccessTokenActions = {
   clearAccessToken: () => void;
 };
 
-export const useAuthStore = create<AccessTokenState & AccessTokenActions>(
-  (set) => ({
-    accessToken: null,
+export const useAuthStore = create<AccessTokenState & AccessTokenActions>()(
+  devtools(
+    (set) => ({
+      accessToken: null,
 
-    setAccessToken: (accessToken) => set({ accessToken }),
-    clearAccessToken: () => set({ accessToken: null }),
-  }),
+      setAccessToken: (accessToken) =>
+        set({ accessToken }, false, 'setAccessToken'),
+      clearAccessToken: () =>
+        set({ accessToken: null }, false, 'clearAccessToken'),
+    }),
+    {
+      name: 'auth-store', // Redux DevTools에서 표시될 스토어 이름
+    },
+  ),
 );
