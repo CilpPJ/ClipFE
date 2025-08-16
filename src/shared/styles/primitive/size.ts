70e2c95021554057f1c60b5d8e27617a5f0c452c
Dobbymin <gmin990624@gmail.com>
2025-08-17 01:56:04 +0900

    feat: 크기 및 높이, 너비 정의에 새로운 값 추가

--- src/shared/styles/primitive/size.ts
+++ src/shared/styles/primitive/size.ts
@@ -5,12 +5,18 @@ export const spacing = {
   4: '1rem', // 16px
   5: '1.25rem', // 20px
   6: '1.5rem', // 24px
+  7: '2rem', // 32px
 } as const;
 
 export const height = {
+  full: '100%',
   xs: '2rem', // 32px
   sm: '2.5rem', // 40px
   md: '3rem', // 48px
   lg: '3.5rem', // 56px
   xl: '4rem', // 64px
+} as const;
+
+export const width = {
+  full: '100%',
 } as const;
