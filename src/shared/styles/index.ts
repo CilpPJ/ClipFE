ddfdfc39d2c22ee52ae21304d5fe1565732078be
Dobbymin <gmin990624@gmail.com>
2025-08-17 01:56:26 +0900

    feat: 테마에 너비 속성 추가

--- src/shared/styles/index.ts
+++ src/shared/styles/index.ts
@@ -1,3 +1,2 @@
-export * from './header';
 export * from './primitive';
 export * from './semantic';
--- src/shared/theme/token/semantic-token.ts
+++ src/shared/theme/token/semantic-token.ts
@@ -6,6 +6,7 @@ import {
   semanticTypography,
   spacing,
   typography,
+  width,
 } from '../../styles';
 
 export const theme = {
@@ -15,6 +16,7 @@ export const theme = {
   },
   spacing,
   height,
+  width,
   typography: {
     ...typography,
     semantic: semanticTypography,
