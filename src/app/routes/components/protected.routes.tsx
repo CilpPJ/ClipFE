b6bf3376e0255ebae107e2dc13e482d7abca2547
Dobbymin <gmin990624@gmail.com>
2025-08-16 20:42:14 +0900

    refactor: 라우트 경로 및 핸들 구조 변경

--- src/app/routes/components/protected.routes.tsx
+++ src/app/routes/components/protected.routes.tsx
@@ -8,59 +8,55 @@ import {
   SettingPage,
   SummaryPage,
 } from '@/pages';
+import { ROUTER_PATH } from '@/shared';
 import { Layout } from '@/widgets';
 
 import { ROUTE_CONFIG } from '../config';
 
 export const protectedRoutes = [
   {
-    path: ROUTE_CONFIG.MAIN.path,
-    element: <Layout type='Main' />,
+    path: ROUTER_PATH.ROOT,
+    element: <Layout />,
     children: [
       {
         index: true,
         element: <MainPage />,
+        handle: { title: ROUTE_CONFIG.MAIN.title, layout: 'Main' },
       },
-    ],
-  },
-  {
-    path: '/',
-    element: <Layout type='Page' />,
-    children: [
       {
         path: ROUTE_CONFIG.CLIP.path,
         element: <ClipPage />,
-        handle: { title: ROUTE_CONFIG.CLIP.title },
+        handle: { title: ROUTE_CONFIG.CLIP.title, layout: 'Page' },
       },
       {
         path: ROUTE_CONFIG.CLIP_DETAIL.path,
         element: <ClipDetailPage />,
-        handle: { title: ROUTE_CONFIG.CLIP_DETAIL.title },
+        handle: { title: ROUTE_CONFIG.CLIP_DETAIL.title, layout: 'Page' },
       },
       {
         path: ROUTE_CONFIG.SEARCH.path,
         element: <SearchPage />,
-        handle: { title: ROUTE_CONFIG.SEARCH.title },
+        handle: { title: ROUTE_CONFIG.SEARCH.title, layout: 'Page' },
       },
       {
         path: ROUTE_CONFIG.FRIEND.path,
         element: <FriendPage />,
-        handle: { title: ROUTE_CONFIG.FRIEND.title },
+        handle: { title: ROUTE_CONFIG.FRIEND.title, layout: 'Page' },
       },
       {
         path: ROUTE_CONFIG.SETTING.path,
         element: <SettingPage />,
-        handle: { title: ROUTE_CONFIG.SETTING.title },
+        handle: { title: ROUTE_CONFIG.SETTING.title, layout: 'Page' },
       },
       {
         path: ROUTE_CONFIG.SUMMARY.path,
         element: <SummaryPage />,
-        handle: { title: ROUTE_CONFIG.SUMMARY.title },
+        handle: { title: ROUTE_CONFIG.SUMMARY.title, layout: 'Page' },
       },
       {
         path: ROUTE_CONFIG.ADD.path,
         element: <AddPage />,
-        handle: { title: ROUTE_CONFIG.ADD.title },
+        handle: { title: ROUTE_CONFIG.ADD.title, layout: 'Page' },
       },
     ],
   },
