7d4eb5dbcc839a3d3ae68ddc84f8041528c63edf
Dobbymin <gmin990624@gmail.com>
2025-08-17 01:56:16 +0900

    remove: 사용하지 않는 파일 제거

--- src/features/main/components/button/SignupButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, DialogTrigger } from '@/shared';
-
-export const SignupButton = () => {
-  return (
-    <DialogTrigger asChild>
-      <Button variant='outline'>회원가입</Button>
-    </DialogTrigger>
-  );
-};
