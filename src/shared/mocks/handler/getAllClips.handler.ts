ec748c38c82f18f0ccf661a41038e04d88ee78ff
Dobbymin <gmin990624@gmail.com>
2025-08-16 15:33:35 +0900

    feat: getAllClipsHandler에서 API 응답 형식 개선 및 서버 시간 추가

--- src/shared/mocks/handler/getAllClips.handler.ts
+++ src/shared/mocks/handler/getAllClips.handler.ts
@@ -1,12 +1,19 @@
 import { HttpResponse, http } from 'msw';
 
 import { ALL_CLIPS_PATH } from '@/features';
-import { BASE_URL } from '@/shared/libs';
 
+import { BASE_URL } from '../../libs';
+import type { ApiResponse } from '../../types';
 import { RECENT_CLIP_CARD_MOCK } from '../data';
 
 export const getAllClipsHandler = [
   http.get(`${BASE_URL}${ALL_CLIPS_PATH}`, async () => {
-    return HttpResponse.json(RECENT_CLIP_CARD_MOCK);
+    const response: ApiResponse<typeof RECENT_CLIP_CARD_MOCK> = {
+      status: 'SUCCESS',
+      data: RECENT_CLIP_CARD_MOCK,
+      serverDateTime: new Date().toISOString(),
+    };
+
+    return HttpResponse.json(response);
   }),
 ];
