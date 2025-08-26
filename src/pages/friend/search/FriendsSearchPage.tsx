6427092245ea5855955f65195b56b64e03b77b64
Dobbymin <gmin990624@gmail.com>
2025-08-26 22:48:25 +0900

    refactor(friend-search): 전체 폴더구조 변경 및 변경사항 적용

--- src/pages/friend/search/FriendsSearchPage.tsx
+++ src/pages/friend/search/FriendsSearchPage.tsx
@@ -1,11 +1,20 @@
-import { FriendsSearchForm } from '@/features/search/components/features/FriendsSearchForm';
-import { UserList } from '@/widgets/user-list';
+import styled from '@emotion/styled';
+
+import { FriendsSearchForm, UserListSection } from '@/features';
 
 export default function FriendsSearchPage() {
   return (
-    <>
+    <FriendsSearchPageLayout>
       <FriendsSearchForm />
-      <UserList />
-    </>
+      <UserListSection />
+    </FriendsSearchPageLayout>
   );
 }
+
+const FriendsSearchPageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 ${({ theme }) => theme.spacing[4]};
+  width: ${({ theme }) => theme.width.full};
+  gap: ${({ theme }) => theme.spacing[5]};
+`;
