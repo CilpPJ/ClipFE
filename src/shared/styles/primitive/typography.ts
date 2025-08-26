8ca953f2990c1fcbd9ef01f9a56cc87602a5447c
Dobbymin <gmin990624@gmail.com>
2025-08-26 22:45:45 +0900

    style: 타이포그래피 lg 값 수정

--- src/shared/styles/primitive/typography.ts
+++ src/shared/styles/primitive/typography.ts
@@ -2,7 +2,7 @@ export const typography = {
   fontSize: {
     xxl: '2rem', // 32px
     xl: '1.5rem', // 24px
-    lg: '1.2rem', // 19.2px
+    lg: '1.125rem', // 18px
     md: '1rem', // 16px
     sm: '0.875rem', // 14px
     xs: '0.75rem', // 12px
